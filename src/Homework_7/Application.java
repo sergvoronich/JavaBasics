e2413eff58d0235c6d44c7bdb94163f4505c111f
Serg <sergvoronich@mail.ru>
2021-07-19 14:00:45 +0300

    Optional task about ATM (Bankomat) added

---
+++ src/Homework_7/Application.java
@@ -0,0 +1,59 @@
+package Homework_7;
+
+public class Application {
+
+    public static void addMoney(int bill20, int bill50, int bill100, Bankomat bankomat) {
+        bankomat.setBill20(bankomat.getBill20() + bill20);
+        bankomat.setBill50(bankomat.getBill50() + bill50);
+        bankomat.setBill100(bankomat.getBill100() + bill100);
+        int sum = bill20 * 20 + bill50 * 50 + bill100 * 100;
+        System.out.println("Добавленная сумма = " + sum);
+        System.out.println("Общая сумма в банкомате = " + bankomat.getTotalSum());
+    }
+
+    public static void withdrawMoney(int sum, Bankomat bankomat) {
+        int col100;
+        int col50;
+        int col20;
+        if(sum <= bankomat.getTotalSum()) {
+            if((sum%50)%20 != 0) {
+                System.out.println("Указанная сумма не может быть выдана, так как в " +
+                        "банкомате присутствуют только купюры номиналом 20, 50 и 100");
+            } else {
+                if(sum <= bankomat.getSum100()) {
+                    col100 = sum / 100;
+                } else {
+                    col100 = bankomat.getBill100();
+                }
+                sum -= col100*100;
+                if(sum <= bankomat.getSum50()) {
+                    col50 = sum / 50;
+                } else {
+                    col50 = bankomat.getBill50();
+                }
+                sum -= col50*50;
+                col20 = sum / 20;
+                System.out.println("Операция успешно выполнена!");
+                System.out.println("Выдано " + col100 + " купюр номиналом 100, " + col50 +
+                        " купюр номиналом 50 и " + col20 + " купюр номиналом 20.");
+            }
+        } else {
+            System.out.println("В банкомате недостаточно средств!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Bankomat bankomat = new Bankomat(100, 50, 20);
+
+        bankomat.printInfo();
+
+        addMoney(10,10 , 10, bankomat);
+
+        withdrawMoney(800, bankomat);
+
+
+
+
+    }
+
+}
---
+++ src/Homework_7/Bankomat.java
@@ -0,0 +1,65 @@
+package Homework_7;
+
+public class Bankomat {
+    private int bill20;
+    private int bill50;
+    private int bill100;
+
+    public int getSum20() {
+        return getBill20()*20;
+    }
+
+    public int getSum50() {
+        return getBill50()*50;
+    }
+
+    public int getSum100() {
+        return getBill100()*100;
+    }
+
+    public int getTotalSum() {
+        return getBill20()*20 + getBill50()*50 + getBill100()*100;
+    }
+
+    public void printInfo() {
+        System.out.println();
+        System.out.println("Количество купюр номиналом 20 = " + getBill20());
+        System.out.println("Количество купюр номиналом 50 = " + getBill50());
+        System.out.println("Количество купюр номиналом 100 = " + getBill100());
+        System.out.println("Сумма купюрами номиналом 20 = " + getSum20());
+        System.out.println("Сумма купюрами номиналом 50 = " + getSum50());
+        System.out.println("Сумма купюрами номиналом 100 = " + getSum100());
+        System.out.println("Общая сумма в банкомате = " + getTotalSum());
+        System.out.println();
+    }
+
+    public Bankomat(int bill20, int bill50, int bill100) {
+        this.bill20 = bill20;
+        this.bill50 = bill50;
+        this.bill100 = bill100;
+    }
+
+    public int getBill20() {
+        return bill20;
+    }
+
+    public void setBill20(int bill20) {
+        this.bill20 = bill20;
+    }
+
+    public int getBill50() {
+        return bill50;
+    }
+
+    public void setBill50(int bill50) {
+        this.bill50 = bill50;
+    }
+
+    public int getBill100() {
+        return bill100;
+    }
+
+    public void setBill100(int bill100) {
+        this.bill100 = bill100;
+    }
+}
